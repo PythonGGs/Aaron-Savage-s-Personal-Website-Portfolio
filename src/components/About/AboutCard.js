384d852c3114f01870ac4133dfadd02ee46ab199
PythonGGs <88223007+PythonGGs@users.noreply.github.com>
2024-02-07 09:52:35 -0500

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,33 +8,32 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Aaron Savage </span>
+            from <span className="purple"> Dallas Gerogia.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently a Marketing Manager. 
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+              I have over 100 million views of generated from my Management! 
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+             Apart from Managing , some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Playing Fortnite
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Watching TV
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Reading Books related to Business
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Strive to help people and make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Savage</footer>
         </blockquote>
       </Card.Body>
     </Card>
